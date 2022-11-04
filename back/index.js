c0c7ea44787f6b6d885e95abe647f51ce9c6d969
高明昊 <gaoryrt@baicizhan.com>
2022-11-04 15:48:13 +0800

    fix: 🐛 更新远程

--- back/index.js
+++ back/index.js
@@ -34,17 +34,12 @@ app.get('/getLastData', async (req, res) => {
 
 app.post('/saveTemp', async (req, res) => {
   const {door, val, ts} = req.body
-  const now = new Date(ts * 1000)
   const db = await connectToDatabase(uri)
   const collection = await db.collection('wmcollection')
   const [{ history }] = await collection.find({ door }).toArray()
-  const last = history.sort((a, b) => b.ts - a.ts)[0]
-  const lastVal = last.val
-  const lastTs = new Date(Math.max(last.ts, 1640880000) * 1000)
+  const lastVal = history.sort((a, b) => b.ts - a.ts)[0].val
   const data = await collection.updateOne({ door }, { $set: { temp: {val, ts} }})
-  const wprice = new Decimal(val).minus(lastVal).mul(3.1)
-  const gprice = ((now.getYear() - lastTs.getYear()) * 12 + now.getMonth() - lastTs.getMonth()) * 8
-  get(`http://localhost:8080/dak4o5tFLtqPVwsUGtpXiK/%23${door}/%E5%BA%94%E4%BB%98%E6%AC%BE%C2%A5${wprice.plus(gprice)}%EF%BC%8C%E8%AF%B7%E7%A1%AE%E8%AE%A4%E6%94%B6%E6%AC%BE?url=https://42.gaoryrt.com/water/confirmTemp?door=${door}&icon=https://cdn.jsdelivr.net/gh/gaoryrt/f/202111161522629.png`)
+  get(`http://localhost:8080/dak4o5tFLtqPVwsUGtpXiK/%23${door}/%E5%BA%94%E4%BB%98%E6%AC%BE%C2%A5${new Decimal(val).minus(lastVal).mul(3.1)}%EF%BC%8C%E8%AF%B7%E7%A1%AE%E8%AE%A4%E6%94%B6%E6%AC%BE?url=https://42.gaoryrt.com/water/confirmTemp?door=${door}&icon=https://cdn.jsdelivr.net/gh/gaoryrt/f/202111161522629.png`)
   res.send(data)
 })
 
@@ -58,8 +53,7 @@ app.get('/confirmTemp', async (req, res) => {
   else if (lastTs === temp.ts) res.send({ error: 'temp 已经保存过了' })
   else {
     const data = await collection.updateOne({ door }, { $set: { temp: null, history: [...history, temp] }})
-    if (data.modifiedCount === 1) res.send('已入库')
-    else res.send(data)
+    res.send(data)
   }
 })
 
